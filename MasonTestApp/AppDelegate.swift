2650165c9c35d3c50e0ae3a50806c337db5e6c51
Mason Mark <mason@fivespeed.com>
2016-03-19 11:17:18 +0900

    Initial Commit

---
+++ MasonTestApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MasonTestApp
+//
+//  Created by mason on 2016-03-19.
+//  Copyright © 2016 MASONMARK.COM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
